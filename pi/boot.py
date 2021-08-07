0a579961536c01e9775c1f291502e1992d5855f9
Maarten Van Steenkiste <maarten@starfisk.com>
2021-08-07 22:27:54 +0200

    Comment out 'remote debugging'

--- pi/boot.py
+++ pi/boot.py
@@ -9,7 +9,7 @@ opt.add_argument("--disable-extensions")
 opt.add_argument("--headless")
 opt.add_argument('window-size=1920x1080')
 opt.add_argument('use-fake-ui-for-media-stream')
-opt.add_argument('--remote-debugging-port=9222')
+# opt.add_argument('--remote-debugging-port=9222')
 opt.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
     "profile.default_content_setting_values.media_stream_camera": 1,
