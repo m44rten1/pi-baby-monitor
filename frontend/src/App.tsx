c98e515942019fbafd420aa2f0ab6443270c3e20
Maarten Van Steenkiste <maarten@starfisk.com>
2021-08-13 21:38:18 +0200

    Added signin form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,178 @@
 /* tslint:disable */
 
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import "bulma/css/bulma.min.css";
 import "./App.css";
 
-import Peer from "peerjs";
+import Peer, { util } from "peerjs";
 
-const peer = new Peer(); // random id
-
-// Connect to pi
-peer.on("open", () => {
-  const conn = peer.connect("test_test_test1234567890");
-  console.log("Connection: ", conn);
-  conn.send("hi!");
-  // on open will be launch when you successfully connect to PeerServer
-  conn.on("open", () => {
-    // here you have conn.id
-    console.log("Connected and sending...");
-    conn.send("hi! from iphone network!");
-  });
-
-  conn.on("data", (data) => {
-    console.log("I received:", data);
-  });
-
-  peer.on("call", function (call) {
-    // Answer the call, providing our mediaStream
-    call.answer(undefined);
-
-    call.on("stream", function (stream) {
-      console.log("Raspberry stream: ", stream);
-
-      const video = document.getElementById("video") as HTMLVideoElement;
-
-      if (video) {
-        video.srcObject = stream;
-        video.play();
-        console.log("Video binded...");
-      }
-    });
-  });
-});
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <video id="video"></video>
-        <div>Test</div>
-      </header>
-    </div>
-  );
+declare global {
+  interface Window {
+    peerjs: { util: util };
+  }
 }
+
+type SignInInfo = {
+  id: string;
+  password: string;
+  rememberMe: boolean;
+};
+
+let defaultSignInInfo: SignInInfo = {
+  id: "",
+  password: "",
+  rememberMe: false,
+};
+const SIGN_IN_INFO_KEY = "SIGN_IN_INFO";
+const storedSignInInfo = localStorage.getItem(SIGN_IN_INFO_KEY);
+
+if (storedSignInInfo) {
+  defaultSignInInfo = JSON.parse(storedSignInInfo);
+}
+
+const App = () => {
+  const [connected, setConnected] = useState(false);
+  const [signInInfo, setSignInInfo] = useState(defaultSignInInfo);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSignInInfoChange = <P extends keyof SignInInfo>(
+    prop: P,
+    value: SignInInfo[P]
+  ) => {
+    setSignInInfo({ ...signInInfo, [prop]: value });
+  };
+
+  const connect = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const peer = new Peer();
+    peer.on("open", () => {
+      const conn = peer.connect(signInInfo.id);
+      conn.on("open", () => {
+        conn.send(signInInfo.password);
+      });
+
+      conn.on("data", (data) => {
+        if (data === "wrong") {
+          setIsLoading(false); // TODO: show wrong password message..., add timeout also?
+        }
+
+        if (data === "permission-failure") {
+          setIsLoading(false); // TODO: handle no permissions error...
+        }
+      });
+    });
+
+    peer.on("call", (call) => {
+      call.answer(undefined);
+
+      call.on("stream", (stream) => {
+        setIsLoading(false); // TODO: switch to video view here, unhide video...
+        setConnected(true);
+        if (signInInfo.rememberMe)
+          localStorage.setItem(SIGN_IN_INFO_KEY, JSON.stringify(signInInfo));
+        const video = document.getElementById("video") as HTMLVideoElement;
+
+        if (video) {
+          video.srcObject = stream;
+          video.play();
+        }
+      });
+    });
+  };
+
+  return (
+    <>
+      {connected ? (
+        <></>
+      ) : (
+        <section className="hero is-primary is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                  <form action="" className="box">
+                    <div className="field">
+                      <label className="label">Identifier</label>
+                      <div className="control has-icons-left">
+                        <input
+                          placeholder="my-unique-id-12345!"
+                          className="input"
+                          autoComplete="on"
+                          required
+                          value={signInInfo.id}
+                          onChange={(e) =>
+                            onSignInInfoChange("id", e.target.value)
+                          }
+                          disabled={isLoading}
+                        />
+                        <span className="icon is-small is-left">
+                          <i className="fa fa-id-card"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">Password</label>
+                      <div className="control has-icons-left">
+                        <input
+                          type="password"
+                          placeholder="*******"
+                          className="input"
+                          autoComplete="on"
+                          required
+                          value={signInInfo.password}
+                          onChange={(e) =>
+                            onSignInInfoChange("password", e.target.value)
+                          }
+                          disabled={isLoading}
+                        />
+                        <span className="icon is-small is-left">
+                          <i className="fa fa-lock"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="checkbox">
+                        <input
+                          type="checkbox"
+                          checked={signInInfo.rememberMe}
+                          onChange={() =>
+                            onSignInInfoChange(
+                              "rememberMe",
+                              !signInInfo.rememberMe
+                            )
+                          }
+                        />
+                        <span className="ml-1">Remember me</span>
+                      </label>
+                    </div>
+                    <div className="field">
+                      <button
+                        className={`button is-success ${
+                          isLoading ? "is-loading" : ""
+                        }`}
+                        onClick={connect}
+                      >
+                        Connect
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+
+      <video
+        id="video"
+        hidden={!connected || isLoading}
+        style={{ objectFit: "cover", width: "100vw", height: "100vh" }}
+        controls
+      ></video>
+    </>
+  );
+};
 
 export default App;
